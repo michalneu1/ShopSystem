d921d43d0507033c5ca2355335a0dde4155710d8
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2026-07-11 15:23:15 +0200

    feat: add Client, Order  for Task 5

---
+++ src/main/java/org/example/model/Client.java
@@ -0,0 +1,15 @@
+package org.example.model;
+
+public class Client {
+    private String name;
+    private String lastName;
+    private String mail;
+    private short age;
+
+    public Client(String name, String lastName, short age, String mail) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.mail = mail;
+    }
+}
---
+++ src/main/java/org/example/order/Order.java
@@ -0,0 +1,45 @@
+package org.example.order;
+
+import org.example.cart.CartItem;
+import org.example.model.Client;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Order {
+    private static final AtomicInteger COUNTER = new AtomicInteger(1);
+    private final int id;
+    private final List<CartItem> items;
+    private final BigDecimal value;
+    private final Client client;
+
+    public Order(List<CartItem> items, BigDecimal value, Client client) {
+        this.id = COUNTER.getAndIncrement();
+        this.items = items;
+        this.value = value;
+        this.client = client;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", products=" + items +
+                ", value=" + value +
+                ", client=" + client +
+                '}';
+    }
+}
