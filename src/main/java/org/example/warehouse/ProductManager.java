7b20bfed5e1c61ff7340a8cf6ed158ad39fa0129
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2026-07-11 03:29:57 +0200

    feat: add Warehouse and ProductManager  for Task 2

---
+++ src/main/java/org/example/warehouse/ProductManager.java
@@ -0,0 +1,44 @@
+package org.example.warehouse;
+
+import org.example.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class ProductManager {
+
+    private final WareHouse wareHouse;
+
+    public ProductManager(WareHouse wareHouse) {
+        this.wareHouse = wareHouse;
+    }
+
+    public void addToWareHouse(Product product) {
+        wareHouse.getProducts().add(product);
+    }
+
+    public void removeFromWareHouse(int id) {
+        findByID(id).ifPresent(p -> wareHouse.getProducts().remove(p));
+    }
+
+    public Optional<Product> findByID(int id) {
+        return wareHouse.getProducts().stream().filter(x -> x.getId() == id).findFirst();
+    }
+
+    public void showProducts() {
+        for (Product product : wareHouse.getProducts()) {
+            System.out.println(product);
+        }
+    }
+
+    public void updateProduct(int id, String name, BigDecimal value, int quantity) {
+        Optional<Product> optional = findByID(id);
+        if (optional.isPresent()) {
+            Product product = optional.get();
+            product.setName(name);
+            product.setValue(value);
+            product.setQuantity(quantity);
+        }
+    }
+
+}
